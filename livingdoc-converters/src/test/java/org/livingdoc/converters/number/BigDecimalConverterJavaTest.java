990f9decccfb354f517fc9750dac5a6f42826790
Drakojin <drakojin@gmail.com>
2017-07-24 22:11:37 +0200

    temporary fix for failing bigdecimal test
    fixes for removed default method of TypeConverter

--- livingdoc-converters/src/test/java/org/livingdoc/converters/number/BigDecimalConverterJavaTest.java
+++ livingdoc-converters/src/test/java/org/livingdoc/converters/number/BigDecimalConverterJavaTest.java
@@ -3,7 +3,10 @@ package org.livingdoc.converters.number;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
+import java.util.Locale;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import utils.EnglishDefaultLocale;
@@ -13,6 +16,18 @@ import utils.EnglishDefaultLocale;
 class BigDecimalConverterJavaTest {
 
     BigDecimalConverter cut = new BigDecimalConverter();
+    private Locale defaultLocale;
+
+    @BeforeEach
+    void setup() {
+        defaultLocale = Locale.getDefault();
+        Locale.setDefault(Locale.ENGLISH);
+    }
+
+    @AfterEach
+     void teardown() {
+        Locale.setDefault(defaultLocale);
+    }
 
     @Test
     void converterCanConvertedToJavaBigDecimal() {
