ef7ec7f1e58a180417c69194b1c1776353a05802
Steffen Schneider <schneisn123@gmail.com>
2019-10-29 10:56:27 +0100

    Add @Disabled to ExecutableDocumentDescriptor

--- livingdoc-junit-engine/livingdoc-junit-engine.gradle.kts
+++ livingdoc-junit-engine/livingdoc-junit-engine.gradle.kts
@@ -6,4 +6,5 @@ dependencies {
 	compile(project(":livingdoc-engine"))
 
 	compile("org.junit.platform:junit-platform-engine:${Versions.junitPlatform}")
+	compile("org.junit.jupiter:junit-jupiter-api:${Versions.junitJupiter}")
 }
