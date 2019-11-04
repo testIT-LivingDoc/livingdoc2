be31238fac749717cd86affbf3c40776e00e7edb
Magnus Specht <st142989@stud.uni-stuttgart.de>
2019-11-04 13:56:04 +0100

    Merge disable scenario and decision table tests to fix testing pipeline

--- livingdoc-sample/src/test/kotlin/org/livingdoc/example/CalculatorDocumentDisabledFixtures.kt
+++ livingdoc-sample/src/test/kotlin/org/livingdoc/example/CalculatorDocumentDisabledFixtures.kt
@@ -3,13 +3,17 @@ package org.livingdoc.example
 import org.assertj.core.api.Assertions.assertThat
 import org.livingdoc.api.disabled.Disabled
 import org.livingdoc.api.documents.ExecutableDocument
+import org.livingdoc.api.fixtures.decisiontables.BeforeRow
+import org.livingdoc.api.fixtures.decisiontables.Check
+import org.livingdoc.api.fixtures.decisiontables.DecisionTableFixture
+import org.livingdoc.api.fixtures.decisiontables.Input
 import org.livingdoc.api.fixtures.scenarios.Before
 import org.livingdoc.api.fixtures.scenarios.Binding
 import org.livingdoc.api.fixtures.scenarios.ScenarioFixture
 import org.livingdoc.api.fixtures.scenarios.Step
 
 @ExecutableDocument("local://Calculator.md")
-class CalculatorDocumentDisabledScenarioFixtures {
+class CalculatorDocumentDisabledFixtures {
 
     @Disabled("Disabled ScenarioFixture")
     @ScenarioFixture
@@ -69,6 +73,51 @@ class CalculatorDocumentDisabledScenarioFixtures {
         ) {
             val result = sut.sum(a, a)
             assertThat(result).isEqualTo(b)
+        }
+    }
+
+    @Disabled("Disabled DecisionTableFixture")
+    @DecisionTableFixture
+    class DisabledCalculatorDecisionTableFixture {
+
+        private lateinit var sut: Calculator
+
+        @Input("a")
+        private var valueA: Float = 0f
+        private var valueB: Float = 0f
+
+        @BeforeRow
+        fun beforeRow() {
+            sut = Calculator()
+        }
+
+        @Input("b")
+        fun setValueB(valueB: Float) {
+            this.valueB = valueB
+        }
+
+        @Check("a + b = ?")
+        fun checkSum(expectedValue: Float) {
+            val result = sut.sum(valueA, valueB)
+            assertThat(result).isEqualTo(expectedValue)
+        }
+
+        @Check("a - b = ?")
+        fun checkDiff(expectedValue: Float) {
+            val result = sut.diff(valueA, valueB)
+            assertThat(result).isEqualTo(expectedValue)
+        }
+
+        @Check("a * b = ?")
+        fun checkMultiply(expectedValue: Float) {
+            val result = sut.multiply(valueA, valueB)
+            assertThat(result).isEqualTo(expectedValue)
+        }
+
+        @Check("a / b = ?")
+        fun checkDivide(expectedValue: Float) {
+            val result = sut.divide(valueA, valueB)
+            assertThat(result).isEqualTo(expectedValue)
         }
     }
 }
