e0c6d3ed40f4435d7c7c192e6faaed4a4589fe2b
Sascha Zeller <zeller.dev@gmail.com>
2019-11-08 13:03:02 +0100

    Scenario fixture documentation

---
+++ livingdoc-documentation/src/main/java/examples/scenarios/CalculatorFixture.java
@@ -0,0 +1,29 @@
+package examples.scenarios;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import implementations.Calculator;
+import org.livingdoc.api.fixtures.scenarios.*;
+
+@ScenarioFixture("Calculator")
+public class CalculatorFixture {
+
+    private Calculator sut;
+
+    @Before
+    private void before(){
+        sut = new Calculator();
+    }
+
+    @Step("adding {a} and {b} equals {c}")
+    void add(@Binding("a") Float a, @Binding("b") Float b, @Binding("c") Float c) {
+        double result = sut.sum(a, b);
+        assertThat(result).isEqualTo(c);
+    }
+
+    @After
+    private void after(){
+
+    }
+
+}
