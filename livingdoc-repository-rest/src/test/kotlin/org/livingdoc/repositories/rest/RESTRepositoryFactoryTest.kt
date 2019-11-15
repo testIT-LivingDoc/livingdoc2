133779595a6cc3e08ffe4d04815edce9cb240c66
Jinaz <qqjx3@web.de>
2019-11-15 20:19:36 +0100

    code refactoring and completing

--- livingdoc-repository-rest/src/test/kotlin/org/livingdoc/repositories/rest/RESTRepositoryFactoryTest.kt
+++ livingdoc-repository-rest/src/test/kotlin/org/livingdoc/repositories/rest/RESTRepositoryFactoryTest.kt
@@ -3,18 +3,21 @@ package org.livingdoc.repositories.rest
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class RESTRepositoryFactoryTest {
 
-    /**
-     * manually created multiple repositories
-     */
-    @Test
-    fun `create multiple repositories and compare test`() {
+    lateinit var wms: WireMockServer
+    val reponame = "Testing.html"
+    val rrf: RESTRepositoryFactory = RESTRepositoryFactory()
+
+    @BeforeEach
+    fun startWM() {
 
         // starting server
-        val wms = WireMockServer(8080)
+        wms = WireMockServer(8080)
         wms.stubFor(
             WireMock.get(WireMock.urlEqualTo("/Testing.html")).willReturn(
                 WireMock.aResponse().withBodyFile(
@@ -23,11 +26,36 @@ class RESTRepositoryFactoryTest {
             )
         )
         wms.start()
+    }
+
+    @Test
+    fun `create RESTRepository`() {
+        val comparisonRepository = RESTRepository(reponame, RESTRepositoryConfig())
+        Assertions.assertThat(comparisonRepository.getDocument(reponame)).isNotNull
+
+        // verification
+        wms.verify(WireMock.getRequestedFor(WireMock.urlEqualTo("/Testing.html")))
+    }
+
+    @Test
+    fun `create REST repository via Factory`() {
+        // testing factory
+        val configData: Map<String, Any> = mutableMapOf<String, Any>("baseURL" to "http://localhost:8080/")
+        val resultrepo = rrf.createRepository(reponame, configData)
+
+        Assertions.assertThat(resultrepo.getDocument(reponame)).isNotNull
+
+        // verification
+        wms.verify(WireMock.getRequestedFor(WireMock.urlEqualTo("/Testing.html")))
+    }
+
+    /**
+     * manually created multiple repositories
+     */
+    @Test
+    fun `create multiple repositories and compare test`() {
 
         // testing factory
-        val reponame = "Testing.html"
-        val rrf: RESTRepositoryFactory
-        rrf = RESTRepositoryFactory()
         val configData: Map<String, Any> = mutableMapOf<String, Any>("baseURL" to "http://localhost:8080/")
         val resultrepo = rrf.createRepository(reponame, configData)
 
@@ -35,11 +63,16 @@ class RESTRepositoryFactoryTest {
         val comparisonRepository = RESTRepository(reponame, RESTRepositoryConfig())
 
         // comparison tests
+        Assertions.assertThat(resultrepo.getDocument(reponame)).isNotNull
+        Assertions.assertThat(comparisonRepository.getDocument(reponame)).isNotNull
         Assertions.assertThat(resultrepo.getDocument(reponame).elements.size).isEqualTo(comparisonRepository.getDocument(reponame).elements.size)
 
         // verification
         wms.verify(WireMock.getRequestedFor(WireMock.urlEqualTo("/Testing.html")))
+        }
 
+    @AfterEach
+    fun stopWM() {
         // stopping server
         wms.stop()
     }
