5c90af2c6c2584031cd8526b84ac24d37ecb06f2
Steffen Schneider <schneisn123@gmail.com>
2019-11-19 11:04:51 +0100

    Use wiremock to check for existing file

--- livingdoc-repository-rest/src/test/docs/RestRepositoryIntegrationTest.md
+++ livingdoc-repository-rest/src/test/docs/RestRepositoryIntegrationTest.md
@@ -1,7 +1,15 @@
 # LivingDoc
 
+This is the integration test for the REST-API.
+
+Host has to have a protocol defined (http:// or https://).
+Path has to start with /.
+File-Path is the path to the file that WireMock serves.
+If you do not want to serve a file, pick a path that does not exist.
+
 Examples
 
-| Rest-Url | Throws RestDocumentNotFoundException |
-|-----------|-----------------|
-| http://thislivingdocurldoesnotexist.com | True |
+| Host | Port | Path | File-Path | Throws RestDocumentNotFoundException |
+|------|------|------|------|------------------------------------|
+| http://thislivingdocdoesnotexist.com | 0 | /NoFile.html | | True |
+| http://localhost | 8000 | /Testing.html | Testing.html | False |
--- livingdoc-repository-rest/src/test/kotlin/org/livingdoc/repositories/rest/RestRepositoryIntegrationTest.kt
+++ livingdoc-repository-rest/src/test/kotlin/org/livingdoc/repositories/rest/RestRepositoryIntegrationTest.kt
@@ -1,9 +1,11 @@
 package org.livingdoc.repositories.rest
 
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.livingdoc.api.documents.ExecutableDocument
-import org.livingdoc.api.fixtures.decisiontables.BeforeRow
 import org.livingdoc.api.fixtures.decisiontables.Check
 import org.livingdoc.api.fixtures.decisiontables.DecisionTableFixture
 import org.livingdoc.api.fixtures.decisiontables.Input
@@ -15,27 +17,57 @@ class RestRepositoryIntegrationTest {
     class FileRepositoryDecisionTableFixture {
 
         private lateinit var cut: RESTRepository
+        lateinit var wms: WireMockServer
 
-        @Input("Rest-Url")
-        private var restUrl: String = ""
+        @Input("Host")
+        private var host: String = ""
 
-        @BeforeRow
-        fun beforeRow() {
+        @Input("Port")
+        private var port: Int = 8080
+
+        @Input("Path")
+        private var path: String = ""
+
+        @Input("File-Path")
+        private var filePath: String = ""
+
+        // Input is not yet initialized when using @beforeRow,
+        // so this function has to be manually called before every test.
+        private fun beforeCheck() {
             val restRepositoryConfig = RESTRepositoryConfig()
-            restRepositoryConfig.baseURL = restUrl
+            restRepositoryConfig.baseURL = "$host:$port"
             cut = RESTRepository("", restRepositoryConfig)
+
+            wms = WireMockServer(port)
+            wms.start()
+            WireMock.configureFor(host, wms.port())
+
+            wms.stubFor(
+                WireMock.get(WireMock.urlEqualTo(path)).willReturn(
+                    WireMock.aResponse().withBodyFile(
+                        filePath
+                    )
+                )
+            )
+        }
+
+        @AfterEach
+        fun afterRow() {
+            wms.stop()
         }
 
         @Check("Throws RestDocumentNotFoundException")
         fun checkOutput(expectedValue: Boolean) {
+            this.beforeCheck()
+
             if (expectedValue) {
                 assertThrows<RESTDocumentNotFoundException> {
-                    cut.getDocument(restUrl)
+                    cut.getDocument(path)
                 }
                 return
             }
             assertDoesNotThrow {
-                cut.getDocument(restUrl)
+                cut.getDocument(path)
             }
         }
     }
