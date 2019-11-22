6af942299ba3521c241534ab4f8784faa31bf2d1
Steffen Schneider <schneisn123@gmail.com>
2019-11-22 12:29:21 +0100

    Use wiremock to test the content of html file

--- livingdoc-repository-rest/src/test/kotlin/org/livingdoc/repositories/rest/RestRepositoryIntegrationTest.kt
+++ livingdoc-repository-rest/src/test/kotlin/org/livingdoc/repositories/rest/RestRepositoryIntegrationTest.kt
@@ -2,6 +2,8 @@ package org.livingdoc.repositories.rest
 
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
@@ -9,15 +11,22 @@ import org.livingdoc.api.documents.ExecutableDocument
 import org.livingdoc.api.fixtures.decisiontables.Check
 import org.livingdoc.api.fixtures.decisiontables.DecisionTableFixture
 import org.livingdoc.api.fixtures.decisiontables.Input
+import org.livingdoc.api.fixtures.scenarios.Before
+import org.livingdoc.api.fixtures.scenarios.Binding
+import org.livingdoc.api.fixtures.scenarios.ScenarioFixture
+import org.livingdoc.api.fixtures.scenarios.Step
+import org.livingdoc.repositories.Document
+import org.livingdoc.repositories.model.decisiontable.DecisionTable
+import org.livingdoc.repositories.model.scenario.Scenario
 
 @ExecutableDocument("local://RestRepositoryIntegrationTest.md")
 class RestRepositoryIntegrationTest {
 
     @DecisionTableFixture
-    class FileRepositoryDecisionTableFixture {
+    class RestRepositoryDecisionTableFixture {
 
         private lateinit var cut: RESTRepository
-        lateinit var wms: WireMockServer
+        private lateinit var wms: WireMockServer
 
         @Input("Host")
         private var host: String = ""
@@ -69,6 +78,78 @@ class RestRepositoryIntegrationTest {
             assertDoesNotThrow {
                 cut.getDocument(path)
             }
+        }
+    }
+
+    @ScenarioFixture
+    class RestRepositoryScenarioFixture {
+
+        private lateinit var doc: Document
+
+        @Before
+        fun before() {
+            // Starting server
+            val wms = WireMockServer(WireMockConfiguration.options().dynamicHttpsPort().dynamicPort())
+            wms.start()
+            WireMock.configureFor("localhost", wms.port())
+
+            // Set REST Repository
+            val cfg = RESTRepositoryConfig()
+            cfg.baseURL = "http://localhost:${wms.port()}/"
+            val cut = RESTRepository("test", cfg)
+
+            wms.stubFor(
+                WireMock.get(WireMock.urlEqualTo("/TTT/Testing.html")).willReturn(
+                    WireMock.aResponse().withBodyFile(
+                        "Testing.html"
+                    )
+                )
+            )
+
+            // Get document
+            doc = cut.getDocument("TTT/Testing.html")
+
+            // Stop wms
+            wms.stop()
+        }
+
+        @Step("Example {exampleIndex} should {doesExist}")
+        fun checkExampleExistence(
+            @Binding("exampleIndex") exampleIndex: Int,
+            @Binding("doesExist") doesExist: String
+        ) {
+            when (doesExist) {
+                "exist" -> assertThat(doc.elements[exampleIndex]).isNotNull
+                "not exist" -> assertThat(doc.elements.size).isLessThanOrEqualTo(exampleIndex)
+            }
+        }
+
+        @Step("Example {exampleIndex} should be a {exampleType}")
+        fun checkExampleType(
+            @Binding("exampleIndex") exampleIndex: Int,
+            @Binding("exampleType") exampleType: String
+        ) {
+            when (exampleType) {
+                "DecisionTable" -> assertThat(doc.elements[exampleIndex]).isInstanceOf(DecisionTable::class.java)
+                "Scenario" -> assertThat(doc.elements[exampleIndex]).isInstanceOf(Scenario::class.java)
+            }
+        }
+
+        @Step("Example {exampleIndex} should have {nrRows} rows")
+        fun checkNrRows(
+            @Binding("exampleIndex") exampleIndex: Int,
+            @Binding("nrRows") nrRows: Int
+        ) {
+            assertThat((doc.elements[exampleIndex] as DecisionTable).rows).hasSize(nrRows)
+        }
+
+        @Step("Row {rowIndex} of example {exampleIndex} should have {nrHeaders} headers")
+        fun checkNrHeaders(
+            @Binding("rowIndex") rowIndex: Int,
+            @Binding("exampleIndex") exampleIndex: Int,
+            @Binding("nrHeaders") nrHeaders: Int
+        ) {
+            assertThat((doc.elements[exampleIndex] as DecisionTable).rows[rowIndex].headerToField).hasSize(nrHeaders)
         }
     }
 }
