3ac61fc47fbe5c275b6f70fe6e279edfe9893597
Magnus Specht <st142989@stud.uni-stuttgart.de>
2019-12-02 14:06:16 +0100

    Add Builders for ScenarioResult and StepResult

--- livingdoc-engine/src/main/kotlin/org/livingdoc/engine/execution/examples/scenarios/model/ScenarioResult.kt
+++ livingdoc-engine/src/main/kotlin/org/livingdoc/engine/execution/examples/scenarios/model/ScenarioResult.kt
@@ -2,17 +2,54 @@ package org.livingdoc.engine.execution.examples.scenarios.model
 
 import org.livingdoc.engine.execution.Status
 import org.livingdoc.engine.execution.examples.TestDataResult
+import org.livingdoc.engine.fixtures.Fixture
 import org.livingdoc.repositories.model.scenario.Scenario
 
-data class ScenarioResult(
+data class ScenarioResult private constructor(
     val steps: List<StepResult>,
-    var status: Status = Status.Unknown
+    val status: Status,
+    val fixture: Fixture<Scenario>,
+    val scenario: Scenario
 ) : TestDataResult {
+    class Builder {
+        private var status: Status = Status.Unknown
+        private var steps: MutableList<StepResult> = ArrayList()
+        private var fixture: Fixture<Scenario>? = null
+        private var scenario: Scenario? = null
 
-    companion object {
-        fun from(scenario: Scenario): ScenarioResult {
-            val stepResults = scenario.steps.map { (value) -> StepResult(value) }
-            return ScenarioResult(stepResults)
+        fun withStatus(status: Status): Builder {
+            this.status = status
+            return this
+        }
+
+        fun withStep(step: StepResult): Builder {
+            steps.add(step)
+            return this
+        }
+
+        fun ofFixture(fixture: Fixture<Scenario>): Builder {
+            this.fixture = fixture
+            return this
+        }
+
+        fun ofScenario(scenario: Scenario): Builder {
+            this.scenario = scenario
+            return this
+        }
+
+        fun build(): ScenarioResult {
+            // TODO validate result
+            return when {
+                this.fixture == null -> {
+                    throw IllegalArgumentException("Cant't build ScenarioResult without a fixture")
+                }
+                this.scenario == null -> {
+                    throw IllegalArgumentException("Cant't build ScenarioResult without a scenario")
+                }
+                else -> {
+                    ScenarioResult(this.steps, this.status, this.fixture!!, this.scenario!!)
+                }
+            }
         }
     }
 }
--- livingdoc-engine/src/main/kotlin/org/livingdoc/engine/execution/examples/scenarios/model/StepResult.kt
+++ livingdoc-engine/src/main/kotlin/org/livingdoc/engine/execution/examples/scenarios/model/StepResult.kt
@@ -1,8 +1,43 @@
 package org.livingdoc.engine.execution.examples.scenarios.model
 
 import org.livingdoc.engine.execution.Status
+import java.lang.reflect.Method
+import java.util.*
 
-data class StepResult(
+data class StepResult private constructor(
     val value: String,
-    var status: Status = Status.Unknown
-)
+    val status: Status,
+    val fixtureMethod: Optional<Method>
+) {
+    class Builder {
+        private var status: Status = Status.Unknown
+        private var value: String = ""
+        private var fixtureMethod: Method? = null
+
+        fun withStatus(status: Status): Builder {
+            this.status = status
+            return this
+        }
+
+        fun withValue(value: String): Builder {
+            this.value = value
+            return this
+        }
+
+        fun withFixtureMethod(fixtureMethod: Method): Builder {
+            this.fixtureMethod = fixtureMethod
+            return this
+        }
+
+        fun build(): StepResult {
+            return when {
+                this.fixtureMethod == null -> {
+                    StepResult(this.value, this.status, Optional.empty())
+                }
+                else -> {
+                    StepResult(this.value, this.status, Optional.of(this.fixtureMethod!!))
+                }
+            }
+        }
+    }
+}
