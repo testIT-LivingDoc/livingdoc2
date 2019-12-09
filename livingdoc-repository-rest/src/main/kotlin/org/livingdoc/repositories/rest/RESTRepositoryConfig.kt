823631dc172a315530a67ef952224488fec01caf
Markus Haug <haugms@studi.informatik.uni-stuttgart.de>
2019-12-09 14:55:55 +0100

    Add documentation to the livingdoc-repository-rest project

--- livingdoc-repository-rest/src/main/kotlin/org/livingdoc/repositories/rest/RESTRepositoryConfig.kt
+++ livingdoc-repository-rest/src/main/kotlin/org/livingdoc/repositories/rest/RESTRepositoryConfig.kt
@@ -1,5 +1,8 @@
 package org.livingdoc.repositories.rest
 
+/**
+ * This class implements the configuration for a REST repository
+ */
 class RESTRepositoryConfig {
     var baseURL: String = "http://localhost/"
 }
