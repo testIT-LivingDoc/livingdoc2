dd0f032c7e6848e45894233932d2e3ec0d0408dc
Jingxi <qqjx3@web.de>
2019-12-13 17:47:38 +0100

    use parameterized tests

--- livingdoc-engine/src/test/kotlin/org/livingdoc/engine/execution/examples/scenarios/matching/StemmerHandlerTest.kt
+++ livingdoc-engine/src/test/kotlin/org/livingdoc/engine/execution/examples/scenarios/matching/StemmerHandlerTest.kt
@@ -2,7 +2,15 @@ package org.livingdoc.engine.execution.examples.scenarios.matching
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
+import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
+
+@TestInstance(PER_CLASS)
 class StemmerHandlerTest {
     val string: String = "hello world"
 
@@ -24,124 +32,63 @@ class StemmerHandlerTest {
             .isEqualTo("hello world")
     }
 
-    @Test
-    fun `stemming tests words part1`() {
-        Assertions.assertThat(StemmerHandler.stemWords("deny"))
-            .isEqualTo("deni")
-        Assertions.assertThat(StemmerHandler.stemWords("declining"))
-            .isEqualTo("declin")
-        Assertions.assertThat(StemmerHandler.stemWords("diversity"))
-            .isEqualTo("diversit")
-        Assertions.assertThat(StemmerHandler.stemWords("divers"))
-            .isEqualTo("diver")
-        Assertions.assertThat(StemmerHandler.stemWords("dental"))
-            .isEqualTo("dental")
+    /**
+     * provides values for parameterized test for stemming algorithm
+     */
+    fun valueProvider(): Stream<Arguments> {
+        return Stream.of(
+            Arguments.of("deny", "deni"),
+            Arguments.of("declining", "declin"),
+            Arguments.of("diversity", "diversit"),
+            Arguments.of("divers", "diver"),
+            Arguments.of("dental", "dental"),
+            Arguments.of("caress", "caress"),
+            Arguments.of("cats", "cat"),
+            Arguments.of("feed", "feed"),
+            Arguments.of("agreed", "agre"),
+            Arguments.of("disabled", "disabl"),
+            Arguments.of("matting", "mat"),
+            Arguments.of("mating", "mate"),
+            Arguments.of("meeting", "meet"),
+            Arguments.of("milling", "mill"),
+            Arguments.of("messing", "mess"),
+            Arguments.of("meetings", "meet"),
+            Arguments.of("mappization", "mappiz"),
+            Arguments.of("sensational", "sensat"),
+            Arguments.of("grenci", "grenci"),
+            Arguments.of("granci", "granci"),
+            Arguments.of("neutralizer", "neutral"),
+            Arguments.of("betional", "betional"),
+            Arguments.of("doubli", "doubli"),
+            Arguments.of("dalli", "dalli"),
+            Arguments.of("rentli", "rentli"),
+            Arguments.of("reli", "reli"),
+            Arguments.of("bousli", "bousli"),
+            Arguments.of("liberation", "liber"),
+            Arguments.of("grundization", "grundiz"),
+            Arguments.of("senator", "senat"),
+            Arguments.of("malism", "malism"),
+            Arguments.of("liveness", "live"),
+            Arguments.of("fulness", "ful"),
+            Arguments.of("gousness", "gous"),
+            Arguments.of("taliti", "taliti"),
+            Arguments.of("leiviti", "leiviti"),
+            Arguments.of("enbiliti", "enbl"),
+            Arguments.of("physiologi", "physiolog"),
+            Arguments.of("delicate", "delic"),
+            Arguments.of("decorative", "decor"),
+            Arguments.of("meiciti", "meiciti"),
+            Arguments.of("insightful", "insight"),
+            Arguments.of("clearance", "clearanc"),
+            Arguments.of("scientific", "scientific"),
+            Arguments.of("reliable", "reliabl"),
+            Arguments.of("hence", "henc"),
+            Arguments.of("ant", "ant")
+            )
     }
-    @Test
-    fun `stemming tests words part2`() {
-        Assertions.assertThat(StemmerHandler.stemWords("caresses"))
-            .isEqualTo("caress")
-        Assertions.assertThat(StemmerHandler.stemWords("ponies"))
-            .isEqualTo("poni")
-        Assertions.assertThat(StemmerHandler.stemWords("ties"))
-            .isEqualTo("ti")
-        Assertions.assertThat(StemmerHandler.stemWords("caress"))
-            .isEqualTo("caress")
-        Assertions.assertThat(StemmerHandler.stemWords("cats"))
-            .isEqualTo("cat")
-
-        Assertions.assertThat(StemmerHandler.stemWords("feed"))
-            .isEqualTo("feed")
-        Assertions.assertThat(StemmerHandler.stemWords("agreed"))
-            .isEqualTo("agre")
-        Assertions.assertThat(StemmerHandler.stemWords("disabled"))
-            .isEqualTo("disabl")
-
-        Assertions.assertThat(StemmerHandler.stemWords("matting"))
-            .isEqualTo("mat")
-        Assertions.assertThat(StemmerHandler.stemWords("mating"))
-            .isEqualTo("mate")
-        Assertions.assertThat(StemmerHandler.stemWords("meeting"))
-            .isEqualTo("meet")
-        Assertions.assertThat(StemmerHandler.stemWords("milling"))
-            .isEqualTo("mill")
-        Assertions.assertThat(StemmerHandler.stemWords("messing"))
-            .isEqualTo("mess")
-
-        Assertions.assertThat(StemmerHandler.stemWords("meetings"))
-            .isEqualTo("meet")
-    }
-    @Test
-    fun `stemming tests words part3`() {
-        Assertions.assertThat(StemmerHandler.stemWords("mappization"))
-            .isEqualTo("mappiz")
-        Assertions.assertThat(StemmerHandler.stemWords("sensational"))
-            .isEqualTo("sensat")
-        Assertions.assertThat(StemmerHandler.stemWords("grenci"))
-            .isEqualTo("grenci")
-        Assertions.assertThat(StemmerHandler.stemWords("granci"))
-            .isEqualTo("granci")
-        Assertions.assertThat(StemmerHandler.stemWords("neutralizer"))
-            .isEqualTo("neutral")
-        Assertions.assertThat(StemmerHandler.stemWords("betional"))
-            .isEqualTo("betional")
-
-        Assertions.assertThat(StemmerHandler.stemWords("doubli"))
-            .isEqualTo("doubli")
-        Assertions.assertThat(StemmerHandler.stemWords("dalli"))
-            .isEqualTo("dalli")
-        Assertions.assertThat(StemmerHandler.stemWords("rentli"))
-            .isEqualTo("rentli")
-        Assertions.assertThat(StemmerHandler.stemWords("reli"))
-            .isEqualTo("reli")
-        Assertions.assertThat(StemmerHandler.stemWords("bousli"))
-            .isEqualTo("bousli")
-        Assertions.assertThat(StemmerHandler.stemWords("liberation"))
-            .isEqualTo("liber")
-        Assertions.assertThat(StemmerHandler.stemWords("grundization"))
-            .isEqualTo("grundiz")
-        Assertions.assertThat(StemmerHandler.stemWords("senator"))
-            .isEqualTo("senat")
-        Assertions.assertThat(StemmerHandler.stemWords("malism"))
-            .isEqualTo("malism")
-        Assertions.assertThat(StemmerHandler.stemWords("liveness"))
-            .isEqualTo("live")
-        Assertions.assertThat(StemmerHandler.stemWords("fulness"))
-            .isEqualTo("ful")
-        Assertions.assertThat(StemmerHandler.stemWords("gousness"))
-            .isEqualTo("gous")
-    }
-
-    @Test
-    fun `stemming test words part4`() {
-        Assertions.assertThat(StemmerHandler.stemWords("taliti"))
-            .isEqualTo("taliti")
-        Assertions.assertThat(StemmerHandler.stemWords("leiviti"))
-            .isEqualTo("leiviti")
-
-        Assertions.assertThat(StemmerHandler.stemWords("enbiliti"))
-            .isEqualTo("enbl")
-        Assertions.assertThat(StemmerHandler.stemWords("physiologi"))
-            .isEqualTo("physiolog")
-        Assertions.assertThat(StemmerHandler.stemWords("delicate"))
-            .isEqualTo("delic")
-        Assertions.assertThat(StemmerHandler.stemWords("decorative"))
-            .isEqualTo("decor")
-        Assertions.assertThat(StemmerHandler.stemWords("meiciti"))
-            .isEqualTo("meiciti")
-        Assertions.assertThat(StemmerHandler.stemWords("logical"))
-            .isEqualTo("logic")
-        Assertions.assertThat(StemmerHandler.stemWords("insightful"))
-            .isEqualTo("insight")
-        Assertions.assertThat(StemmerHandler.stemWords("clearance"))
-            .isEqualTo("clearanc")
-        Assertions.assertThat(StemmerHandler.stemWords("scientific"))
-            .isEqualTo("scientific")
-        Assertions.assertThat(StemmerHandler.stemWords("reliable"))
-            .isEqualTo("reliabl")
-        Assertions.assertThat(StemmerHandler.stemWords("hence"))
-            .isEqualTo("henc")
-        Assertions.assertThat(StemmerHandler.stemWords("ant"))
-            .isEqualTo("ant")
+    @ParameterizedTest
+    @MethodSource("valueProvider")
+    fun `stemming tests words`(input: String, expectedValue: String) {
+        Assertions.assertThat(StemmerHandler.stemWords(input)).isEqualTo(expectedValue)
     }
 }
