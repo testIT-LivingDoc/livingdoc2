fe712abd0b525462cd19cf3f4a0cf1af996d57c5
Steffen Schneider <schneisn123@gmail.com>
2019-12-15 17:04:51 +0100

    Change comment to kdoc

--- livingdoc-api/src/main/java/org/livingdoc/api/exception/ExampleSyntax.java
+++ livingdoc-api/src/main/java/org/livingdoc/api/exception/ExampleSyntax.java
@@ -1,7 +1,9 @@
 package org.livingdoc.api.exception;
 
 public class ExampleSyntax {
-    // The string that the business expert defines in the Example to indicate
-    // an exception is expected to be thrown.
+    /**
+     * The string that the business expert defines in the Example to indicate
+     * an exception is expected to be thrown.
+     */
     public static final String EXCEPTION = "error";
 }
