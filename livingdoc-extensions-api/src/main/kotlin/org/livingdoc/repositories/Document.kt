e6caf9a33f0bdc1d59ee21574bb0d1df199a42e2
Markus Haug <haugms@studi.informatik.uni-stuttgart.de>
2019-12-16 12:07:33 +0100

    Add documentation to parts of repositories

--- livingdoc-extensions-api/src/main/kotlin/org/livingdoc/repositories/Document.kt
+++ livingdoc-extensions-api/src/main/kotlin/org/livingdoc/repositories/Document.kt
@@ -2,4 +2,10 @@ package org.livingdoc.repositories
 
 import org.livingdoc.repositories.model.TestData
 
+/**
+ * Document represents the [TestData] contained in a document fetched from a [DocumentRepository].
+ *
+ * @see DocumentRepository
+ * @see TestData
+ */
 open class Document(val elements: List<TestData>)
