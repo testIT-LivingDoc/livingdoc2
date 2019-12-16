94569406017986b20693c8e1f7ffcd877ae4301f
Markus Haug <haugms@studi.informatik.uni-stuttgart.de>
2019-12-16 09:28:18 +0100

    Add documentation to file repository project

--- livingdoc-repository-file/src/main/kotlin/org/livingdoc/repositories/file/DocumentFile.kt
+++ livingdoc-repository-file/src/main/kotlin/org/livingdoc/repositories/file/DocumentFile.kt
@@ -1,7 +1,14 @@
 package org.livingdoc.repositories.file
 
 import java.io.File
+import org.livingdoc.repositories.Document
 
+/**
+ * A DocumentFile wraps a [File] that contains a [Document]
+ *
+ * @see Document
+ * @see File
+ */
 open class DocumentFile(private val file: File) {
     open fun extension() = file.extension
     open fun stream() = file.inputStream()
--- livingdoc-repository-file/src/main/kotlin/org/livingdoc/repositories/file/FileRepositoryFactory.kt
+++ livingdoc-repository-file/src/main/kotlin/org/livingdoc/repositories/file/FileRepositoryFactory.kt
@@ -3,6 +3,14 @@ package org.livingdoc.repositories.file
 import org.livingdoc.config.YamlUtils
 import org.livingdoc.repositories.DocumentRepositoryFactory
 
+/**
+ * A FileRepositoryFactory is a [DocumentRepositoryFactory] used to create [FileRepository] instances with a specific
+ * [FileRepositoryConfig].
+ *
+ * @see DocumentRepositoryFactory
+ * @see FileRepository
+ * @see FileRepositoryConfig
+ */
 class FileRepositoryFactory : DocumentRepositoryFactory<FileRepository> {
 
     override fun createRepository(name: String, configData: Map<String, Any>): FileRepository {
