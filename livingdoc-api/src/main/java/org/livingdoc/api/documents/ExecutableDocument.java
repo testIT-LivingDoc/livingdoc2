d5eb5ed6ebd48cfc4e664fc288f2746fbf426d37
Markus Haug <haugms@studi.informatik.uni-stuttgart.de>
2020-01-11 16:25:56 +0100

    Clarify single group rule

--- livingdoc-api/src/main/java/org/livingdoc/api/documents/ExecutableDocument.java
+++ livingdoc-api/src/main/java/org/livingdoc/api/documents/ExecutableDocument.java
@@ -17,8 +17,13 @@ public @interface ExecutableDocument {
     /**
      * The {@link Group} that this ExecutableDocument belongs too.
      * <p>
-     * Note that if this class is nested inside a group it is an error to specify this attribute to be different from
-     * the parent class
+     * This augments the usual lookup functionality, in which an ExecutableDocument that is declared nested inside a
+     * class annotated with {@link Group} is considered part of that group.
+     * </p>
+     * <p>
+     * Note that an ExecutableDocument can only belong to a single group. It is an error if the {@link Group} specified
+     * by this attribute on the annotation and the {@link Group} discovered by the nested class lookup are different.
+     * </p>
      *
      * @see Group
      */
