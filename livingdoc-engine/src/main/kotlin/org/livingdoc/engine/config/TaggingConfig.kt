06ad8a25600cdf81658a698363f701bdcf492ff5
Matthias Gürtler <matthias-fabian.guertler@t-online.de>
2020-02-17 15:30:13 +0100

    fix property for commandline

--- livingdoc-engine/src/main/kotlin/org/livingdoc/engine/config/TaggingConfig.kt
+++ livingdoc-engine/src/main/kotlin/org/livingdoc/engine/config/TaggingConfig.kt
@@ -2,13 +2,15 @@ package org.livingdoc.engine.config
 
 import org.livingdoc.config.ConfigProvider
 
-data class TaggingConfig(private val tags: TaggingDefinition = TaggingDefinition()) {
+data class TaggingConfig(var tags: TaggingDefinition = TaggingDefinition()) {
     companion object {
         fun from(configProvider: ConfigProvider): TaggingConfig {
             return configProvider.getConfigAs("tags", TaggingConfig::class)
         }
     }
 
-    val includedTags = System.getProperty("livingdoc.tags.include")?.split(',') ?: tags.include
-    val excludedTags = System.getProperty("livingdoc.tags.exclude")?.split(',') ?: tags.exclude
+    val includedTags : List<String>
+        get() = System.getProperty("livingdoc.tags.include")?.split(',') ?: tags.include
+    val excludedTags : List<String>
+        get() = System.getProperty("livingdoc.tags.exclude")?.split(',') ?: tags.exclude
 }
