96eca7728ce45327ca3a88ac18e5bf4cf9131abb
Markus Haug <haugms@studi.informatik.uni-stuttgart.de>
2020-03-04 12:04:48 +0100

    Simplify ConfluenceLink class

--- livingdoc-reports/src/main/kotlin/org/livingdoc/reports/confluence/tree/elements/ConfluenceLink.kt
+++ livingdoc-reports/src/main/kotlin/org/livingdoc/reports/confluence/tree/elements/ConfluenceLink.kt
@@ -2,6 +2,11 @@ package org.livingdoc.reports.confluence.tree.elements
 
 import org.livingdoc.reports.html.elements.HtmlElement
 
+/**
+ * A link element in a Confluence page tree report
+ *
+ * @param target The title of the page the link is pointing to
+ */
 class ConfluenceLink(target: String) : HtmlElement("ac:link") {
     init {
         child {
@@ -9,16 +14,5 @@ class ConfluenceLink(target: String) : HtmlElement("ac:link") {
                 attr("ri:content-title", target)
             }
         }
-    }
-
-    /**
-     * A link element in a Confluence page tree report
-     *
-     * @param target The address the link is pointing to
-     * @param block A lambda generating the content of this link
-     */
-    constructor(target: String, block: ConfluenceLink.() -> Unit) :
-            this(target) {
-        block()
     }
 }
