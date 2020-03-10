78261dcde0e07274927d2e0971b09260c1d9d258
Leon Kiefer <leon.k97@gmx.de>
2020-03-10 23:50:00 +0100

    fixed git repository setup with new engine

--- livingdoc-repository-git/livingdoc-repository-git.gradle.kts
+++ livingdoc-repository-git/livingdoc-repository-git.gradle.kts
@@ -8,10 +8,10 @@ repositories {
 
 dependencies {
 	implementation(project(":livingdoc-config"))
-	implementation(project(":livingdoc-extensions-api"))
+	implementation(project(":livingdoc-api"))
 	implementation(project(":livingdoc-repositories"))
 
-	implementation(group = "org.eclipse.jgit", name = "org.eclipse.jgit", version = Versions.jgit)
+	implementation("org.eclipse.jgit:org.eclipse.jgit:${Versions.jgit}")
 
 	testImplementation("ch.qos.logback:logback-classic:${Versions.logback}")
 	testImplementation("org.assertj:assertj-core:${Versions.assertJ}")
